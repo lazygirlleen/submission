aeb5af812d6eda9d0fe4586fc5a15e9bf6e3e2dc
Sulis <146931473+lazygirlleen@users.noreply.github.com>
2025-03-15 12:32:34 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import os
 import streamlit as st
 import pandas as pd
@@ -30,9 +31,14 @@ start_date = st.sidebar.date_input("Tanggal Mulai", df_day["dteday"].min())
 end_date = st.sidebar.date_input("Tanggal Akhir", df_day["dteday"].max())
 
 # Pilih musim
-season_options = {1: "Spring", 2: "Summer", 3: "Fall", 4: "Winter"}
+season_options = {1: "Semi", 2: "Panas", 3: "Gugur", 4: "Dingin"}
 selected_season = st.sidebar.selectbox("Pilih Musim", season_options.values())
 selected_season_key = list(season_options.keys())[list(season_options.values()).index(selected_season)]
+
+# Pilih cuaca
+season_options = {1: "Cerah", 2: "Berawan", 3: "Gerimis", 4: "Hujan"}
+selected_weather = st.sidebar.selectbox("Pilih Cuaca", weather_options.values())
+selected_weather_key = list(weather_options.keys())[list(weather_options.values()).index(selected_weather)]
 
 # Filter data berdasarkan pilihan
 filtered_df_day = df_day[
